c8f590be95763ceedaa023d90d460fc2e4463cde
ahmad.abidin <ahmad.abidin.whh@gmail.com>
2020-08-25 08:32:20 +0700

    change tools

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Simple Microservice : e-commerce (not completed yet)
 View Design [Here](./design/README.md)
 
+tools :
+Angular :
+[@improbable-eng/grpc-web](https://www.npmjs.com/package/@improbable-eng/grpc-web)
+[ts-protoc-gen](https://www.npmjs.com/package/ts-protoc-gen)
+
 generate proto angular and go:
 protoc \
-    --go_out=plugins=grpc:./[directory_on_server] \
-    --plugin=protoc-gen-ts=./[directory_on_client(web)]/node_modules/.bin/protoc-gen-ts \
-    --ts_out=service=true:./[directory_on_client(web)]/src/app \
-    --js_out=import_style=commonjs,binary:./[directory_on_client(web)]/src/app \
-    ./proto/[file_proto.proto]
+    --go_out=plugins=grpc:./auth-service \
+    --plugin=protoc-gen-ts=./e-commerce-web/node_modules/.bin/protoc-gen-ts \
+    --ts_out=service=true:./e-commerce-web/src/app \
+    --js_out=import_style=commonjs,binary:./e-commerce-web/src/app \
+    ./proto/auth.proto
---
+++ e-commerce-web/src/app/proto/auth_pb_service.d.ts
@@ -0,0 +1,82 @@
+// package: proto
+// file: proto/auth.proto
+
+import * as proto_auth_pb from "../proto/auth_pb";
+import {grpc} from "@improbable-eng/grpc-web";
+
+type AuthAuthentication = {
+  readonly methodName: string;
+  readonly service: typeof Auth;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof proto_auth_pb.Identity;
+  readonly responseType: typeof proto_auth_pb.Credential;
+};
+
+type AuthAuthorization = {
+  readonly methodName: string;
+  readonly service: typeof Auth;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof proto_auth_pb.Credential;
+  readonly responseType: typeof proto_auth_pb.FullIdentity;
+};
+
+export class Auth {
+  static readonly serviceName: string;
+  static readonly Authentication: AuthAuthentication;
+  static readonly Authorization: AuthAuthorization;
+}
+
+export type ServiceError = { message: string, code: number; metadata: grpc.Metadata }
+export type Status = { details: string, code: number; metadata: grpc.Metadata }
+
+interface UnaryResponse {
+  cancel(): void;
+}
+interface ResponseStream<T> {
+  cancel(): void;
+  on(type: 'data', handler: (message: T) => void): ResponseStream<T>;
+  on(type: 'end', handler: (status?: Status) => void): ResponseStream<T>;
+  on(type: 'status', handler: (status: Status) => void): ResponseStream<T>;
+}
+interface RequestStream<T> {
+  write(message: T): RequestStream<T>;
+  end(): void;
+  cancel(): void;
+  on(type: 'end', handler: (status?: Status) => void): RequestStream<T>;
+  on(type: 'status', handler: (status: Status) => void): RequestStream<T>;
+}
+interface BidirectionalStream<ReqT, ResT> {
+  write(message: ReqT): BidirectionalStream<ReqT, ResT>;
+  end(): void;
+  cancel(): void;
+  on(type: 'data', handler: (message: ResT) => void): BidirectionalStream<ReqT, ResT>;
+  on(type: 'end', handler: (status?: Status) => void): BidirectionalStream<ReqT, ResT>;
+  on(type: 'status', handler: (status: Status) => void): BidirectionalStream<ReqT, ResT>;
+}
+
+export class AuthClient {
+  readonly serviceHost: string;
+
+  constructor(serviceHost: string, options?: grpc.RpcOptions);
+  authentication(
+    requestMessage: proto_auth_pb.Identity,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: proto_auth_pb.Credential|null) => void
+  ): UnaryResponse;
+  authentication(
+    requestMessage: proto_auth_pb.Identity,
+    callback: (error: ServiceError|null, responseMessage: proto_auth_pb.Credential|null) => void
+  ): UnaryResponse;
+  authorization(
+    requestMessage: proto_auth_pb.Credential,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: proto_auth_pb.FullIdentity|null) => void
+  ): UnaryResponse;
+  authorization(
+    requestMessage: proto_auth_pb.Credential,
+    callback: (error: ServiceError|null, responseMessage: proto_auth_pb.FullIdentity|null) => void
+  ): UnaryResponse;
+}
+
